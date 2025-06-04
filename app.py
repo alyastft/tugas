3e45e0abefdffcf58dada38f8aeba3e02a935ca7
alyastft <alyaastft@students.unnes.ac.id>
2025-06-04 16:33:47 +0700

    app.py

--- app.py
+++ app.py
@@ -20,8 +20,23 @@ if page == "Data Description":
 
 # Page 2: Prediction
 elif page == "Prediction":
-    st.title("Prediction")
-    st.write("This is the prediction page")
+    st.title("Prediksi Jenis Bunga Iris 🌸")
+    st.write("Masukkan data yang akan diprediksi:")
+
+    # Input fitur dari pengguna
+    seplen = st.number_input("Sepal Length", min_value=0.0, max_value=10.0, value=2.0)
+    sepwid = st.number_input("Sepal Width", min_value=0.0, max_value=10.0, value=2.0)
+    petlen = st.number_input("Petal Length", min_value=0.0, max_value=10.0, value=2.0)
+    petwid = st.number_input("Petal Width", min_value=0.0, max_value=10.0, value=2.0)
+
+    # Prediksi saat tombol ditekan
+    if st.button("Prediksi"):
+    input_data = pd.DataFrame([[seplen, sepwid, petlen, petwid]],
+                              columns=["sepal.length", "sepal.width", "petal.length", "petal.width"])
+    st.dataframe(input_data)
+
+    hasil = model.predict(input_data)
+    st.success(f"Hasil Prediksi: {hasil[0]}")
 
 # Page 3: About Naive Bayes
 elif page == "About Naive Bayes":
